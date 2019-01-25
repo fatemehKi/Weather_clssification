07c82e71dbfa7666eca6948a66fc4b1a84b636d9
Fatemeh Kiaie <40069260+fatemehKi@users.noreply.github.com>
2019-01-25 10:18:06 -0500

    Update scikit_learn_weather_data.py

--- scikit_learn_weather_data.py
+++ scikit_learn_weather_data.py
@@ -2,3 +2,22 @@ import pandas as pd
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
+
+#Creating a Pandas DataFrame from the weathear CSV file
+data = pd.read_csv('./daily_weather.csv')
+# looking at the data frame
+data
+
+#cleaning the data (there are some null values and also removing the number column
+del data['number']
+
+########Now let's drop null values using the *pandas dropna* function.
+#check if it does have a null rows
+data[data.isnull().any(axis=1)]
+#storing data before delet and null
+before_rows = data.shape[0]
+print(before_rows)
+#dropinging the null values
+data = data.dropna()
+after_rows = data.shape[0]
+print(after_rows)
